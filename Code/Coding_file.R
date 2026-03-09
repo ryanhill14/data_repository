cde74d7e8eabf09112541675c4c26001ec92c44e
Hill <Ryan.Hill2@atkinsrealis.com>
2026-03-09 11:48:15 +0000

    Toy data and plot

--- Code/Coding_file.R
+++ Code/Coding_file.R
@@ -4,3 +4,24 @@ data_doubled
 
 # Aiming to plot this data
 # Correct, this would be a good idea
+
+# ---- Generate toy data ----
+set.seed(123)                     # for reproducibility
+x <- 1:50                         # x-axis values
+y <- cumsum(rnorm(50, 0, 1))      # random walk for something visually interesting
+toy_data <- data.frame(x, y)
+
+# ---- Plot and save as PNG ----
+png("toy_line_plot.png", width = 800, height = 600)   # open PNG device
+
+plot(
+  toy_data$x, toy_data$y,
+  type = "l",
+  col = "steelblue",
+  lwd = 2,
+  xlab = "Index",
+  ylab = "Value",
+  main = "Toy Data Line Plot"
+)
+
+dev.off()   # close PNG device
